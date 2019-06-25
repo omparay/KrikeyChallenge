c30df7bcb242aa2ecf6bde08506e93a3efe660c7
Oliver Paray <omparay@gmail.com>
2019-06-24 21:45:50 -0700

    Initial UI

---
+++ iDeviceApp/iDeviceApp/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  iDeviceApp
+//
+//  Created by Oliver Paray on 6/24/19.
+//  Copyright © 2019 Oliver Paray. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func retunToSearch(_ unwindSegue:UIStoryboardSegue){
+
+    }
+}
+
