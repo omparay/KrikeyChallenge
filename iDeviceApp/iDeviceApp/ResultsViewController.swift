8d0e58f0a9a8a8c9ee17f6e8b06c563d17590fd5
Oliver Paray <omparay@gmail.com>
2019-06-26 01:53:14 -0700

    Working results view controller

--- iDeviceApp/iDeviceApp/ResultsViewController.swift
+++ iDeviceApp/iDeviceApp/ResultsViewController.swift
@@ -36,6 +36,28 @@ class ResultsViewController: UITableViewController {
         }
     }
 
+    func getItemAtIndex(_ index: Int) -> JSON? {
+        guard let results = self.ResultsToDisplay,
+            let items = results[iTunesSearch.ResultKeys.results.rawValue] as? [JSON] else {
+                return nil
+        }
+        if items.count > index {
+            return items[index]
+        } else {
+            return nil
+        }
+    }
+
+    func discernPrice(_ item:JSON) -> String?{
+        var result: String?
+        if let price = item[iTunesSearch.ResultKeys.CommonKeys.price.rawValue] {
+            result = "\(price)"
+        } else if let trackPrice = item[iTunesSearch.ResultKeys.CommonKeys.trackPrice.rawValue] {
+            result = "\(trackPrice)"
+        }
+        return result
+    }
+
     // MARK: Delegates
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -52,15 +74,46 @@ class ResultsViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ResultsViewController.resultItemCell, for: indexPath)
+
+        let imageView = cell.contentView.viewWithTag(1) as? UIImageView
+        let kindLabel = cell.contentView.viewWithTag(2) as! UILabel
+        let artistLabel = cell.contentView.viewWithTag(3) as! UILabel
+        let titleLabel = cell.contentView.viewWithTag(4) as! UILabel
+        let priceLabel = cell.contentView.viewWithTag(5) as! UILabel
+
+        if let item = self.getItemAtIndex(indexPath.row){
+            kindLabel.text = item[iTunesSearch.ResultKeys.CommonKeys.kind.rawValue] as? String
+            artistLabel.text = item[iTunesSearch.ResultKeys.CommonKeys.artistName.rawValue] as? String
+            titleLabel.text = item[iTunesSearch.ResultKeys.CommonKeys.trackCensoredName.rawValue] as? String
+            priceLabel.text = self.discernPrice(item)
+            if let imageUrl = item[iTunesSearch.ResultKeys.CommonKeys.artworkUrl100.rawValue] as? String{
+                let client = HttpClient()
+                client.execute(serviceUrl: imageUrl, webMethod: .Get, executionHandler:
+                    (success: {
+                        (httpResponse, httpData) in
+                        if let data = httpData, let image = UIImage(data: data), let view = imageView {
+                            DispatchQueue.main.async {
+                                view.image = image
+                            }
+                        }
+                    }, failure: {
+                        (httpResponse, systemError, errorMessage) in
+                        if let view = imageView {
+                            DispatchQueue.main.async {
+                                view.image = nil
+                            }
+                        }
+                    }
+                ))
+            }
+        }
+
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let results = self.ResultsToDisplay, let items = results[iTunesSearch.ResultKeys.results.rawValue] as? [JSON] else {
-            return
-        }
-        if items.count > indexPath.row {
-            self.selectedResult = items[indexPath.row]
+        if let item = self.getItemAtIndex(indexPath.row) {
+            self.selectedResult = item
             self.performSegue(withIdentifier: Segues.toDetails.rawValue, sender: self)
         }
     }
